6560629d31c4ee5d203775df29a1659ddaac838f
Gary Franco <gary.franco.cespedes@gmail.com>
2025-06-20 00:55:00 -0400

    feat(): update system script

--- scripts/update-system.sh
+++ scripts/update-system.sh
@@ -7,8 +7,9 @@ echo "🔄 Actualizando índices de paquetes..."
 sudo apt-get update -y
 
 echo "📦 Actualizando paquetes del sistema (manteniendo tus configuraciones existentes)..."
-sudo apt-get -o Dpkg::Options::="--force-confdef" \
-             -o Dpkg::Options::="--force-confold" \
-             upgrade -y
+sudo apt-get -y -q \
+    -o Dpkg::Options::="--force-confdef" \
+    -o Dpkg::Options::="--force-confold" \
+    dist-upgrade
 
 echo "✅ Sistema actualizado sin intervención manual."
